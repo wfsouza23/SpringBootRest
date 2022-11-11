49a957bb603f1b3d08201bc38777d10a90ac6057
Wesley de Souza <wfsouza23@gmail.com>
2022-11-11 16:25:27 -0300

    ajuste no projeto

---
+++ src/main/java/com/example/wesley/WesleyApplication.java
@@ -0,0 +1,13 @@
+package com.example.wesley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WesleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WesleyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wesley/WesleyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wesley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WesleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
