135572affbda9b22763803fd7a13e621d8cd971c
Wesley de Souza <wfsouza23@gmail.com>
2022-11-26 19:52:22 -0300

    delete para Clientes

--- src/main/java/com/example/wesley/services/ClienteService.java
+++ src/main/java/com/example/wesley/services/ClienteService.java
@@ -59,7 +59,7 @@ public class ClienteService {
             repo.deleteById(id);
         }
         catch (DataIntegrityViolationException e){
-            throw new DataIntegrityException("Nao é possivel excluir porque há entidades relacionadas");
+            throw new DataIntegrityException("Nao é possivel excluir porque há pedidos relacionados");
         }
 
     }
