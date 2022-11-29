6400938a8cbac646a6d2aa76e84fba64cbdbb2d3
Wesley de Souza <wfsouza23@gmail.com>
2022-11-28 22:39:13 -0300

    email update validation

--- src/main/java/com/example/wesley/dto/ClienteDTO.java
+++ src/main/java/com/example/wesley/dto/ClienteDTO.java
@@ -1,12 +1,14 @@
 package com.example.wesley.dto;
 
 import com.example.wesley.domain.Cliente;
+import com.example.wesley.services.validation.ClienteUpdate;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
     private static final long serialVersionUID = 1l;
 
--- src/main/java/com/example/wesley/services/validation/ClienteUpdateValidator.java
+++ src/main/java/com/example/wesley/services/validation/ClienteUpdateValidator.java
@@ -1,46 +1,47 @@
 package com.example.wesley.services.validation;
 
-import ch.qos.logback.core.net.server.Client;
 import com.example.wesley.domain.Cliente;
+import com.example.wesley.dto.ClienteDTO;
 import com.example.wesley.dto.ClienteNewDTO;
 import com.example.wesley.enums.TipoCliente;
 import com.example.wesley.repositories.ClienteRepository;
 import com.example.wesley.resources.exceptions.FieldMessage;
 import com.example.wesley.services.validation.utils.BR;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 
-public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+    @Autowired
+    private HttpServletRequest request;
 
     @Autowired
     private ClienteRepository repo;
 
     @Override
-    public void initialize(ClienteInsert ann) {
+    public void initialize(ClienteUpdate ann) {
     }
 
     @Override
-    public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+    public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+
+        Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+
+        Integer uriId = Integer.parseInt(map.get("id"));
 
         List<FieldMessage> list = new ArrayList<>();
 
-        if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
-            list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
-        }
-
-        if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
-            list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
-        }
-
         Cliente aux = repo.findByEmail(objDto.getEmail());
 
-        if(aux != null){
+        if(aux != null && !aux.getId().equals(uriId)){
             list.add(new FieldMessage("email", "Email já existente"));
         }
 
